9c3cbf19490efbb92328ea43da41b29664e721b1
Matteo Infantino <matteo.infantino@widegroup.eu>
2024-12-18 14:20:03 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Benvenuto nel nostro sito di test</h1>
+        <p>Scopri di più su di noi!</p>
+      </header>
+
+      <section>
+        <h2>Chi siamo</h2>
+        <p>blablabl.</p>
+      </section>
+
+      <section>
+        <h2>Servizi</h2>
+        <ul>
+          <li>blabla</li>
+          <li>blabla</li>
+          <li>blablabla</li>
+        </ul>
+      </section>
+
+      <footer>
+        <p>&copy; 2024</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
